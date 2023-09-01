85a02a94cbe3bfe3ce66caa4c0a9fd5682b5afd5
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-01 11:33:25 -0500

    agrago info

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Ingresar por pantalla 2 números y calcular las 4 operaciones aritméticas con 
+//dichos números. Imprimir en pantalla el resultado de cada operación.
+//Realizar un algoritmo que me pida 3 números por pantalla, con el primer número
+//hallar el cuadrado, con el segundo número hallar la raíz cuadrada y 
+//con el tercer número hallar la raíz cúbica. Mostrar en pantalla los 3 resultados aritméticos.
